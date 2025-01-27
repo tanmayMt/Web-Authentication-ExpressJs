23dec59910f606a777727956cec9f0f7d9568553
web-collab <tanmoy587@gmail.com>
2025-01-27 20:07:29 +0530

    Connect with mongoDb

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+//connect mongodb
+mongoose
+  .connect(MONGO_URL)
+  .then(()=>{
+    console.log("mongodb is connected")
+  })
+  .catch((error)=>{
+    console.log(error);
+    process.exit(1);
+  })
 
 
 //Home Route
