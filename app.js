d6e21b300363e9833d3f6763fbd05a7f07267588
web-collab <tanmoy587@gmail.com>
2025-01-27 20:32:38 +0530

    registter route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user.model");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,11 +18,30 @@ mongoose
     process.exit(1);
   })
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Home Route
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/./views/index.html");
 });
+
+app.post("/register",async(req,res)=>{
+    try{
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).send({
+            success:true,
+            message:"Registeration Successfull",
+            data:newUser
+        })
+    }
+    catch(error){
+    res.status(500).json(error.message);
+  }
+})
+
+
 
 // route not found error
 app.use((req, res, next) => {
